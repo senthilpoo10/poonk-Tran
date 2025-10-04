64fa37e7d0c9817b2481e1d37d09a790e058bf72
senthilpoo10 <senthilpoo10@gmail.com>
2025-10-04 20:08:47 +0300

    Delete pong-app/frontend/src directory

--- pong-app/frontend/src/components/lobby/index.ts
+++
@@ -1,5 +0,0 @@
-// frontend/src/components/general/index.ts
-export { MatchHistoryTab } from "./MatchHistoryTab";
-export { MyLockerTab } from "./MyLockerTab";
-export { OverviewTab } from "./OverviewTab";
-export { RallySquadTab } from "./RallySquadTab";
